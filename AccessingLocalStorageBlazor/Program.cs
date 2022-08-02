5cf7cb3fcc6df812397316e8877ac0e5c20d21f7
MrHoran8120 <garyhoran@users.noreply.github.com>
2022-08-02 22:50:26 +1000

    Add project files.

---
+++ AccessingLocalStorageBlazor/Program.cs
@@ -0,0 +1,14 @@
+using AccessingLocalStorageBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Note this line is to needed to get localstorage working.
+builder.Services.AddBlazoredLocalStorage();
+await builder.Build().RunAsync();
